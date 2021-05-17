867a478d63565d7d98b570f3833a56ab7a233849
xup6890609 <71679918+xup6890609@users.noreply.github.com>
2021-05-17 11:46:01 +0800

    test

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+   public void NextLevel(string LvName)
+    {
+        SceneManager.LoadScene(LvName);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        print("跑酷ing...");
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ReStartGame()
+    {
+        print("重新啟程");
+        SceneManager.LoadScene("menu");
+}
+public void QuitGame()
+{
+ print("放棄治療");
+ Application.Quit();
+}
+}
