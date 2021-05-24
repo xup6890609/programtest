52df2c7f3086843a6976f05d90251e84e11fdc9e
xup6890609 <xup6890609@gmail.com>
2021-05-24 17:07:07 +0800

    動作測試

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Collision coll;
+    [HideInInspector]
+    public Rigidbody2D rb;
+    private AnimationScript anim;
+
+    [Header("Stats")]
+    public float speed = 10;
+    public float jumpForce = 50;
+    public float slideSpeed = 5;
+    public float wallJumpLerp = 10;
+    public float dashSpeed = 20;
+
+    [Header("Booleans")]
+    public bool canMove;
+    public bool wallGrab;
+    public bool wallJumped;
+    public bool wallSlide;
+    public bool isDashing;
+
+    private bool groundTouch;
+    private bool hasDashed;
+
+    public int side = 1;
+
+    void Start()
+    {
+        coll = GetComponent<Collision>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<AnimationScript>();
+    }
+
+
+}
